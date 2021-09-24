96839fddbdc62076d0ecf60db986223ea215884b
Prabhakaran V <prabhakaran.v@auriss.com>
2021-09-25 02:37:04 +0530

    FE changes - planning react folder structure

---
+++ platform-webapi/src/layouts/DashBoardLayout.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class DashBoardLayout extends Component {
+    render() {
+        return (
+            <div>
+                
+            </div>
+        )
+    }
+}
