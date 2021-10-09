9776bce6f0b00207c3c6559c4f6119bbfbf3794d
Prabhakaran V <prabhakaran.v@auriss.com>
2021-10-09 13:26:05 +0530

    BE Repo

--- platform-webapi/src/pages/gameJourney/GameJourney.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function GameJourney() {
-    return (
-        <div>
-            GameJourney
-        </div>
-    )
-}
--- platform-webapi/src/store/Store.js
+++
@@ -1,8 +0,0 @@
-import { createStore,applyMiddleware } from 'redux';
-import thunk from 'redux-thunk'
-import {composeWithDevTools} from 'redux-devtools-extension'
-import rootReducer from '../reducers/IndexReducer';
-import logger from 'redux-logger';
-const store = createStore(rootReducer,composeWithDevTools(applyMiddleware(logger,thunk)));
-
-export default store;
